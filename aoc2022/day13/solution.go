20dd375ac874db45fca4df07961e0a7921a5fb11
Vitaly Potyarkin <sio.wtf@gmail.com>
2022-12-19 13:26:53 +0300

    Day 13, part 1: My guess is too high...

---
+++ aoc2022/day13/solution.go
@@ -0,0 +1,194 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+const (
+	ListStart     = "["
+	ListEnd       = "]"
+	ListSeparator = ","
+)
+
+const (
+	Less    int = -1
+	Equal       = 0
+	Greater     = 1
+)
+
+type NestedList struct {
+	Value             int
+	Nested            bool
+	Items             []*NestedList
+	Parent            *NestedList
+	parsingInProgress bool
+}
+
+func IntCompare(a, b int) int {
+	switch {
+	case a < b:
+		return Less
+	case a == b:
+		return Equal
+	case a > b:
+		return Greater
+	default:
+		panic("impossible branching")
+	}
+}
+
+func (this *NestedList) Compare(other *NestedList) int {
+	if !this.Nested && !other.Nested {
+		return IntCompare(this.Value, other.Value)
+	}
+	if !this.Nested && other.Nested {
+		if len(other.Items) == 0 {
+			return Greater
+		}
+		return IntCompare(this.Value, other.Items[0].Value)
+	}
+	if this.Nested && !other.Nested {
+		return other.Compare(this) * -1
+	}
+	var item *NestedList
+	var index, result int
+	for index, item = range this.Items {
+		if index >= len(other.Items) {
+			return Greater
+		}
+		result = item.Compare(other.Items[index])
+		if result != Equal {
+			return result
+		}
+	}
+	switch {
+	case len(this.Items) < len(other.Items):
+		return Less
+	case len(this.Items) == len(other.Items):
+		return Equal
+	default:
+		panic("impossible branching")
+	}
+
+}
+
+func (list *NestedList) Grow() (tail *NestedList, err error) {
+	if !list.Nested {
+		return nil, fmt.Errorf("cannot grow leaf node")
+	}
+	tail = &NestedList{Parent: list, Nested: true}
+	list.Items = append(list.Items, tail)
+	return tail, nil
+}
+
+func (list *NestedList) Append(value int) {
+	list.Nested = true
+	list.Items = append(list.Items, &NestedList{Value: value})
+}
+
+func (list *NestedList) String() string {
+	if list == nil {
+		return "<nil>"
+	}
+	if !list.Nested {
+		return strconv.Itoa(list.Value)
+	}
+
+	var build strings.Builder
+	var item *NestedList
+	var index int
+	build.WriteString(ListStart)
+	for index, item = range list.Items {
+		build.WriteString(item.String())
+		if index < len(list.Items)-1 {
+			build.WriteString(ListSeparator)
+		}
+	}
+	build.WriteString(ListEnd)
+	return build.String()
+}
+
+func (list *NestedList) Parse(line string) (err error) {
+	cursor := &Cursor{list: list}
+	return cursor.Parse(line)
+}
+
+type Cursor struct {
+	list    *NestedList
+	pointer *NestedList
+}
+
+func (cursor *Cursor) Parse(line string) (err error) {
+	var chunk string
+	var value int
+	var closed bool
+	for _, chunk = range strings.Split(line, ListSeparator) {
+		switch {
+		case len(chunk) == 0: // noop
+		case closed:
+			return fmt.Errorf("opening bracket is already closed, but received another chunk: %q", chunk)
+		case strings.HasPrefix(chunk, ListStart):
+			if cursor.pointer == nil {
+				cursor.pointer = cursor.list
+				cursor.list.Nested = true
+			} else {
+				cursor.pointer, err = cursor.pointer.Grow()
+				if err != nil {
+					return fmt.Errorf("could not grow list: %w", err)
+				}
+			}
+			chunk = strings.TrimPrefix(chunk, ListStart)
+			err = cursor.Parse(chunk)
+			if err != nil {
+				return fmt.Errorf("could not parse %q: %w", chunk, err)
+			}
+		case strings.HasSuffix(chunk, ListEnd):
+			chunk = strings.TrimSuffix(chunk, ListEnd)
+			err = cursor.Parse(chunk)
+			if err != nil {
+				return fmt.Errorf("could not parse %q: %w", chunk, err)
+			}
+			cursor.pointer = cursor.pointer.Parent
+			if cursor.pointer == nil {
+				closed = true
+			}
+		default:
+			value, err = strconv.Atoi(chunk)
+			if err != nil {
+				return fmt.Errorf("invalid list value %q: %w", chunk, err)
+			}
+			cursor.pointer.Append(value)
+		}
+	}
+	return nil
+}
+
+func part1(filename string) string {
+	var err error
+	var pair [2]*NestedList
+	var index, pairIndex, result, compare int
+	for line := range ReadLines(filename) {
+		if len(line) == 0 {
+			pairIndex++
+			index = 0
+			compare = pair[0].Compare(pair[1])
+			if compare < 1 {
+				result += pairIndex
+			}
+			continue
+		}
+		pair[index] = &NestedList{}
+		err = pair[index].Parse(line)
+		if err != nil {
+			return fmt.Sprintf("parsing failed: %v", err)
+		}
+		index++
+	}
+	return strconv.Itoa(result)
+}
+
+func part2(filename string) string {
+	return ""
+}
---
+++ aoc2022/day13/solution_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"testing"
+
+	"strconv"
+	"strings"
+)
+
+const sample = "sample.txt"
+
+func TestSample(t *testing.T) {
+	tests := []struct {
+		worker func(string) string
+		input  string
+		result string
+	}{
+		{worker: part1, input: sample, result: "13"},
+		{worker: part2, input: sample, result: ""},
+	}
+	for i, test := range tests {
+		got := strings.TrimSpace(test.worker(test.input))
+		expected := strings.TrimSpace(test.result)
+		if got != expected {
+			t.Errorf("sample: part %d expected %q, got %q", i%2+1, expected, got)
+			if strings.Contains(got, "\n") {
+				for pos, char := range got {
+					exp := []rune(expected)[pos]
+					if char != exp {
+						t.Logf(
+							"mismatching character #%d: got %c, expected %c",
+							pos,
+							char,
+							exp,
+						)
+					}
+				}
+			}
+		}
+	}
+}
+
+func TestParsing(t *testing.T) {
+	var files = []string{
+		"sample.txt",
+		"input.txt",
+	}
+	for _, filename := range files {
+		for line := range ReadLines(filename) {
+			if len(line) == 0 {
+				continue
+			}
+			list := &NestedList{}
+			err := list.Parse(line)
+			if err != nil || list.String() != line {
+				t.Errorf("parsing error (%v): %s -> %s", err, line, list.String())
+			}
+		}
+	}
+}
+
+func TestGuessing(t *testing.T) {
+	value, err := strconv.Atoi(part1("input.txt"))
+	if err != nil {
+		t.Errorf("number parsing error: %v", err)
+	}
+	if value >= 5788 {
+		t.Errorf("part 1 guess is too high: %d", value)
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part1(sample)
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part2(sample)
+	}
+}
