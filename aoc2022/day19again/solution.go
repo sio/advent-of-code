e05c29773870d0e16633d90257df326f3008f468
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-02-03 13:18:26 +0300

    Copy test scaffolding from previous days

--- aoc2022/day19again/solution.go
+++ aoc2022/day19again/solution.go
@@ -6,14 +6,14 @@ import (
 )
 
 func part1(filename string) string {
-	var iter CharIterator
+	var iter LineIterator
 	err := iter.Open(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer iter.Close()
 	for iter.Next() {
-		fmt.Printf("> %c\n", iter.Value())
+		fmt.Printf("> %s\n", iter.Value())
 	}
 	return ""
 }
---
+++ aoc2022/day19again/solution_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"testing"
+
+	"strings"
+)
+
+func TestSample(t *testing.T) {
+	tests := []struct {
+		worker func(string) string
+		input  string
+		result string
+	}{
+		{part1, "sample.txt", "33"},
+		{part2, "sample.txt", ""},
+	}
+	for i, test := range tests {
+		got := strings.TrimSpace(test.worker(test.input))
+		want := strings.TrimSpace(test.result)
+		if got != want {
+			t.Errorf("sample: part %d want %q, got %q", i%2+1, want, got)
+			if strings.Contains(got, "\n") {
+				for pos, char := range got {
+					exp := []rune(want)[pos]
+					if char != exp {
+						t.Logf(
+							"mismatching character #%d: got %c, want %c",
+							pos,
+							char,
+							exp,
+						)
+					}
+				}
+			}
+		}
+	}
+}
+
+func BenchmarkPart1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part1("sample.txt")
+	}
+}
+
+func BenchmarkPart2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		part2("sample.txt")
+	}
+}
