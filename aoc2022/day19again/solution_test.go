e748f2b13e46226763c08334fe6b9980ae3979f4
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-02-06 10:44:22 +0300

    Use subtests for solution test

--- aoc2022/day19again/solution_test.go
+++ aoc2022/day19again/solution_test.go
@@ -3,39 +3,37 @@ package main
 import (
 	"testing"
 
+	"fmt"
 	"strings"
 )
 
+var workers = map[string](func(string) string){
+	"part1": part1,
+	"part2": part2,
+}
+
 func TestSample(t *testing.T) {
 	tests := []struct {
-		worker func(string) string
+		worker string
 		input  string
 		result string
 	}{
-		{part1, "sample.txt", "33"}, // input.txt: 1258 is too low
-		{part2, "sample.txt", ""},
-		{part1, "sample2.txt", "72"},
-		{part2, "sample2.txt", ""},
+		{"part1", "sample.txt", "33"}, // input.txt: 1258 is too low
+		{"part2", "sample.txt", ""},
+		{"part1", "sample2.txt", "72"},
 	}
-	for i, test := range tests {
-		got := strings.TrimSpace(test.worker(test.input))
-		want := strings.TrimSpace(test.result)
-		if got != want {
-			t.Errorf("sample: part %d want %q, got %q", i%2+1, want, got)
-			if strings.Contains(got, "\n") {
-				for pos, char := range got {
-					exp := []rune(want)[pos]
-					if char != exp {
-						t.Logf(
-							"mismatching character #%d: got %c, want %c",
-							pos,
-							char,
-							exp,
-						)
-					}
+	for _, test := range tests {
+		t.Run(fmt.Sprintf("%s/%s", test.input, test.worker), func(t *testing.T) {
+			got := strings.TrimSpace(workers[test.worker](test.input))
+			want := strings.TrimSpace(test.result)
+			if got != want {
+				if strings.Contains(got, "\n") {
+					t.Errorf("want:\n%s\n\ngot:\n%s", want, got)
+				} else {
+					t.Errorf("want %q, got %q", want, got)
 				}
 			}
-		}
+		})
 	}
 }
 
