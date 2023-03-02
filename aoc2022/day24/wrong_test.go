b26dd64e62a570a171b94637d7e95f6166e8d29e
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-03-02 18:16:28 +0300

    Day 24: Searching for the error

--- aoc2022/day24/wrong_test.go
+++ aoc2022/day24/wrong_test.go
@@ -15,3 +15,40 @@ func TestWrongValue(t *testing.T) {
 		t.Errorf("answer for input.txt is too high: %d", result)
 	}
 }
+
+func TestSamplePart2(t *testing.T) {
+	basin := &BlizzardBasin{}
+	basin.Load("sample.txt")
+	search := Search{basin: basin}
+
+	var commute int
+	commute += search.ShortestPath(
+		basin.entrance,
+		basin.exit,
+		commute,
+	)
+	want := 18
+	if commute != want {
+		t.Errorf("first trip: got %d, want %d", commute, want)
+	}
+
+	commute += search.ShortestPath(
+		basin.exit,
+		basin.entrance,
+		commute,
+	)
+	want = 18 + 23
+	if commute != want {
+		t.Errorf("second trip: got %d, want %d", commute, want)
+	}
+
+	commute += search.ShortestPath(
+		basin.entrance,
+		basin.exit,
+		commute,
+	)
+	want = 18 + 23 + 13
+	if commute != want {
+		t.Errorf("third trip: got %d, want %d", commute, want)
+	}
+}
