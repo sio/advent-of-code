83e55a69213dbd0f5b0140a190ab768322b7a24e
Vitaly Potyarkin <sio.wtf@gmail.com>
2023-12-13 10:41:52 +0000

    Day 2, part 2: complete

--- aoc2023/src/Day02/Solve.purs
+++ aoc2023/src/Day02/Solve.purs
@@ -25,7 +25,7 @@ day02 =
 
 samples :: List Sample
 samples =
-  ( Sample (Numeric 8) (Empty)
+  ( Sample (Numeric 8) (Numeric 2286)
     """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
@@ -37,6 +37,7 @@ Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
 solve :: Puzzle -> Solution
 solve puzzle = combine (part1 puzzle) (part2 puzzle)
   where
+    games = fromRight Nil $ runParser puzzle (many game)
     part1 puzzle =
       Part log
         $ Numeric
@@ -45,10 +46,25 @@ solve puzzle = combine (part1 puzzle) (part2 puzzle)
         $ games
       where
         ceiling = RGB 12 13 14
-        games = fromRight Nil $ runParser puzzle (many game)
         log = map debug games
         debug game = Info $ (show game) <> " -> " <> (show $ score ceiling game)
-    part2 _ = Part Nil Empty
+    part2 _ =
+      Part Nil
+        $ Numeric
+        $ foldl add zero
+        $ map power
+        $ map (\(Game _ sets) -> fewest sets) games
+
+-- | Part 2: Fewest cubes required to play the game
+fewest :: List RGB -> RGB
+fewest = foldl ceil mempty
+  where
+    ceil :: RGB -> RGB -> RGB
+    ceil (RGB r1 g1 b1) (RGB r2 g2 b2) = RGB (max r1 r2) (max g1 g2) (max b1 b2)
+
+-- | Part 2: Scoring function
+power :: RGB -> Int
+power (RGB r g b) = r * g * b
 
 -- | Single set of RGB cubes
 data RGB = RGB Int Int Int
